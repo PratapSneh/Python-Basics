211165f888dd0f1f7e699369c013dd6dc202ba7b
Sneh Pratap Singh <115708014+PratapSneh@users.noreply.github.com>
2023-03-17 17:43:33 +0530

    Update BubbleSortCode.py

--- BubbleSortCode.py
+++ BubbleSortCode.py
@@ -1,3 +1,4 @@
+#Sorting of list
 lst = [5,7,2,6,4,8,10,45,12,93, 13, 1]
 
 def bubblsort(array):
@@ -15,3 +16,16 @@ def bubblsort(array):
 print('List before sorting', lst)
 bubblsort(lst)
 print('List after sorting', lst)
+
+##Sorting the list of Tupples in Increasing Order by the Last Element in Each Tuple
+
+tpl_list= [(2,5) , (1,2), (2,3), (4,4), (2,1)]
+
+def last(n):
+    return n[-1]
+
+def sort(tupples):
+    return sorted(tupples, key = last)
+
+print('List of tupple before sorting: \n', tpl_list)
+print('List of tupple after sorting: \n ', sort(tpl_list))
